1b16bf7359c96b437eab2249ca7ac07c81646b3d
Sanglok99 <contactlogi@gmail.com>
2026-05-18 13:35:13 +0000

    add more timings

--- src/rpc/external/send_raw_transaction.rs
+++ src/rpc/external/send_raw_transaction.rs
@@ -31,6 +31,12 @@ pub struct SendRawTransactionEpochMetadataTimings {
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct SendRawTransactionEpochUpdateTimings {
+    pub start_ms: u128,
+    pub end_ms: u128,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct SendRawTransactionSignerTimings {
     pub start_ms: u128,
     pub end_ms: u128,
@@ -43,15 +49,59 @@ pub struct SendRawTransactionRedirectRpcTimings {
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct SendRawTransactionClusterUpdateTimings {
+    pub start_ms: u128,
+    pub end_ms: u128,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct SendRawTransactionIssueOCTimings {
+    pub start_ms: u128,
+    pub end_ms: u128,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct SendRawTransactionMerkleTimings {
+    pub start_ms: u128,
+    pub end_ms: u128,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct SendRawTransactionTXPutTimings {
+    pub start_ms: u128,
+    pub end_ms: u128,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct SendRawTransactionOCPutTimings {
+    pub start_ms: u128,
+    pub end_ms: u128,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct SendRawTransactionSyncEpochTXTimings {
+    pub start_ms: u128,
+    pub end_ms: u128,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct SendRawTransactionResponse {
     pub order_commitment: OrderCommitment,
     pub redirect: bool,
     pub handler_timings: SendRawTransactionHandlerTimings,
     pub cluster_metadata_timings: SendRawTransactionClusterMetadataTimings,
     pub epoch_metadata_timings: SendRawTransactionEpochMetadataTimings,
+    pub epoch_update_timings: SendRawTransactionEpochUpdateTimings,
     pub signer_timings: SendRawTransactionSignerTimings,
     pub redirect_rpc_timings: Option<SendRawTransactionRedirectRpcTimings>,
     pub leader_cluster_metadata_timings: Option<SendRawTransactionClusterMetadataTimings>,
+    pub cluster_update_timings: SendRawTransactionClusterUpdateTimings,
+    pub leader_cluster_update_timings: Option<SendRawTransactionClusterUpdateTimings>,
+    pub issue_order_commitment_timings: SendRawTransactionIssueOCTimings,
+    pub merkle_timings: SendRawTransactionMerkleTimings,
+    pub TX_put_timings: SendRawTransactionTXPutTimings,
+    pub OC_put_timings: SendRawTransactionOCPutTimings,
+    pub sync_epoch_TX_timings: SendRawTransactionSyncEpochTXTimings,
 }
 
 fn now_epoch_ms() -> u128 {
@@ -167,6 +217,8 @@ impl RpcParameter<AppState> for SendRawTransaction {
         if is_current_leader { // 현재 노드가 현재 epoch의 리더인 경우
             mut_cluster_metadata.update()?; // release the lock on ClusterMetadata
 
+            let cluster_update_commit_ms = now_epoch_ms();
+
             let cluster_metadata = ClusterMetadata::get(
                 rollup.platform,
                 rollup.liveness_service_provider,
@@ -246,6 +298,10 @@ impl RpcParameter<AppState> for SendRawTransaction {
 
             mut_epoch_metadata.update()?;
 
+            let epoch_update_commit_ms = now_epoch_ms();
+
+            let tx_put_start_ms = now_epoch_ms();
+
             RawEpochTransactionModel::put_with_transaction_hash(
                 &self.rollup_id,
                 &transaction_hash,
@@ -260,13 +316,21 @@ impl RpcParameter<AppState> for SendRawTransaction {
                 self.raw_transaction.clone(),
                 true,
             )?;
+            
+            let tx_put_end_ms = now_epoch_ms();
+            
+            let merkle_start_ms = now_epoch_ms();
 
             let merkle_tree = context.merkle_tree_manager().get(&self.rollup_id).await?;
             let (_, pre_merkle_path) = merkle_tree.add_data(transaction_hash.as_ref()).await;
             drop(merkle_tree);
 
+            let merkle_end_ms = now_epoch_ms();
+
             // rollup.order_commitment_type 뭔지 출력하는 코드
             // tracing::info!("rollup.order_commitment_type: {:?}", rollup.order_commitment_type); // 결과: Sign
+
+            let issue_order_commitment_start_ms = now_epoch_ms();
 
             let order_commitment = issue_order_commitment(
                 context.clone(),
@@ -280,7 +344,15 @@ impl RpcParameter<AppState> for SendRawTransaction {
             )
             .await?;
 
+            let issue_order_commitment_end_ms = now_epoch_ms();
+
+            let OC_put_start_ms = now_epoch_ms();
+
             order_commitment.put(&self.rollup_id, epoch, transaction_order)?;
+
+            let OC_put_end_ms = now_epoch_ms();
+
+            let sync_epoch_tx_start_ms = now_epoch_ms();
 
             sync_epoch_raw_transaction(
                 context.clone(),
@@ -292,6 +364,8 @@ impl RpcParameter<AppState> for SendRawTransaction {
                 order_commitment.clone(),
                 true,
             );
+
+            let sync_epoch_tx_end_ms = now_epoch_ms();
 
             let builder_rpc_url = context.config().builder_rpc_url.clone();
             let cloned_rpc_client = context.rpc_client();
@@ -362,12 +436,41 @@ impl RpcParameter<AppState> for SendRawTransaction {
                     start_ms: epoch_metadata_start_ms,
                     end_ms: epoch_metadata_end_ms,
                 },
+                epoch_update_timings: SendRawTransactionEpochUpdateTimings {
+                    start_ms: epoch_metadata_start_ms,
+                    end_ms: epoch_update_commit_ms,
+                },
                 signer_timings: SendRawTransactionSignerTimings {
                     start_ms: signer_start_ms,
                     end_ms: signer_end_ms,
                 },
                 redirect_rpc_timings: None,
                 leader_cluster_metadata_timings: None,
+                cluster_update_timings: SendRawTransactionClusterUpdateTimings {
+                    start_ms: cluster_metadata_start_ms,
+                    end_ms: cluster_update_commit_ms,
+                },
+                leader_cluster_update_timings: None,
+                issue_order_commitment_timings: SendRawTransactionIssueOCTimings {
+                    start_ms: issue_order_commitment_start_ms,
+                    end_ms: issue_order_commitment_end_ms,
+                },
+                merkle_timings: SendRawTransactionMerkleTimings {
+                    start_ms: merkle_start_ms,
+                    end_ms: merkle_end_ms,
+                },
+                TX_put_timings: SendRawTransactionTXPutTimings {
+                    start_ms: tx_put_start_ms,
+                    end_ms: tx_put_end_ms,
+                },
+                OC_put_timings: SendRawTransactionOCPutTimings {
+                    start_ms: OC_put_start_ms,
+                    end_ms: OC_put_end_ms,
+                },
+                sync_epoch_TX_timings: SendRawTransactionSyncEpochTXTimings {
+                    start_ms: sync_epoch_tx_start_ms,
+                    end_ms: sync_epoch_tx_end_ms,
+                },
             })
         } else {
             // === Not the leader: forward to the leader node ===
@@ -382,6 +485,8 @@ impl RpcParameter<AppState> for SendRawTransaction {
             self.sender_address = Some(tx_orderer_address.clone());
 
             mut_cluster_metadata.update()?; // release the lock on ClusterMetadata
+
+            let cluster_update_commit_ms = now_epoch_ms();
 
             let cluster_metadata = ClusterMetadata::get(
                 rollup.platform,
@@ -455,6 +560,7 @@ impl RpcParameter<AppState> for SendRawTransaction {
                                     end_ms: cluster_metadata_end_ms,
                                 },
                                 epoch_metadata_timings: response.epoch_metadata_timings,
+                                epoch_update_timings: response.epoch_update_timings,
                                 signer_timings: SendRawTransactionSignerTimings {
                                     start_ms: signer_start_ms,
                                     end_ms: signer_end_ms,
@@ -466,6 +572,18 @@ impl RpcParameter<AppState> for SendRawTransaction {
                                 leader_cluster_metadata_timings: Some(
                                     response.cluster_metadata_timings.clone(),
                                 ),
+                                cluster_update_timings: SendRawTransactionClusterUpdateTimings {
+                                    start_ms: cluster_metadata_start_ms,
+                                    end_ms: cluster_update_commit_ms,
+                                },
+                                leader_cluster_update_timings: Some(
+                                    response.cluster_update_timings.clone(),
+                                ),
+                                issue_order_commitment_timings: response.issue_order_commitment_timings,
+                                merkle_timings: response.merkle_timings,
+                                TX_put_timings: response.TX_put_timings,
+                                OC_put_timings: response.OC_put_timings,
+                                sync_epoch_TX_timings: response.sync_epoch_TX_timings,
                             })
                         }
                         Err(error) => {
